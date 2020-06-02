43eefad791d19f2d0f9b4d48ea9180615f16eb7e
Marcelo Boveto Shima <marceloshima@gmail.com>
2020-06-02 00:03:52 -0300

    Fix graddle e2e.

--- generators/app/index.js
+++ generators/app/index.js
@@ -20,9 +20,9 @@ function createGenerator(env) {
         loadConfig() {
           this.packageJson = this.createStorage('package.json');
           this.jhipsterConfig = this.createStorage('.yo-rc.json', 'generator-jhipster');
-          this.scripts = this.packageJson.createStorage('scripts');
           this.clientFramework = this.jhipsterConfig.get('clientFramework') || 'angular';
           this.testFrameworks = this.jhipsterConfig.get('testFrameworks') || [];
+          this.scripts = this.packageJson.createStorage('scripts');
         },
         dependencies() {
           const devDependencies = this.packageJson.createStorage('devDependencies');
@@ -88,33 +88,32 @@ function createGenerator(env) {
           const javaCommonLog = `-Dlogging.level.ROOT=OFF -Dlogging.level.org.zalando=OFF -Dlogging.level.io.github.jhipster=OFF -Dlogging.level.${this.jhipsterConfig.get(
             'packageName'
           )}=OFF`;
+          const javaTestLog =
+            '-Dlogging.level.org.springframework=OFF -Dlogging.level.org.springframework.web=OFF -Dlogging.level.org.springframework.security=OFF';
 
+          this.scripts.set('backend:test', 'npm run ci:backend:test');
+          this.scripts.set('backend:doc:test', 'npm run ci:backend:doc:test');
           const buildTool = this.jhipsterConfig.get('buildTool');
           if (buildTool === 'maven') {
             this.scripts.set('backend:info', './mvnw -ntp enforcer:display-info --batch-mode');
             this.scripts.set(
-              'backend:test',
-              `./mvnw -ntp -P-webpack verify --batch-mode ${javaCommonLog} -Dlogging.level.org.springframework=OFF -Dlogging.level.org.springframework.web=OFF -Dlogging.level.org.springframework.security=OFF`
+              'ci:backend:test',
+              `./mvnw -ntp -P-webpack verify --batch-mode ${javaCommonLog} ${javaTestLog}`
             );
-            this.scripts.set('backend:doc:test', './mvnw -ntp javadoc:javadoc --batch-mode');
+            this.scripts.set('ci:backend:doc:test', './mvnw -ntp javadoc:javadoc --batch-mode');
             this.scripts.set('java:jar', './mvnw -ntp verify -DskipTests -P"$JHI_PROFILE" --batch-mode');
             this.scripts.set('java:war', './mvnw -ntp verify -DskipTests -P"$JHI_PROFILE",war --batch-mode');
+            // Copy the jar and remove old log and pid
+            this.scripts.set('preci:server:start', 'cp target/*.jar target/e2e.jar');
           } else if (buildTool === 'gradle') {
+            this.scripts.set('ci:backend:test', `./gradlew test integrationTest ${javaCommonLog} ${javaTestLog}`);
             this.scripts.set('backend:info', './gradlew -v');
-            this.scripts.set(
-              'backend:test',
-              `./gradlew test integrationTest ${javaCommonLog} -Dlogging.level.org.springframework=OFF -Dlogging.level.org.springframework.web=OFF -Dlogging.level.org.springframework.security=OFF`
-            );
-            this.scripts.set('backend:doc:test', './gradlew javadoc');
+            this.scripts.set('ci:backend:doc:test', './gradlew javadoc');
             this.scripts.set('java:jar', './gradlew bootJar -P"$JHI_PROFILE" -x test');
             this.scripts.set('java:war', './gradlew bootWar -P"$JHI_PROFILE" -Pwar -x test');
+            this.scripts.set('preci:server:start', 'cp build/libs/*SNAPSHOT.jar app.jar');
           }
 
-          // Copy the jar and remove old log and pid
-          this.scripts.set(
-            'preci:server:start',
-            'cp target/*.jar target/e2e.jar && rm target/server.log target/server.pid || true'
-          );
           this.scripts.set(
             'ci:server:start',
             `java -jar target/e2e.jar --spring.profiles.active="$JHI_PROFILE" ${javaCommonLog} --logging.level.org.springframework.web=ERROR`
@@ -139,9 +138,8 @@ function createGenerator(env) {
           } else {
             this.scripts.set('ci:e2e', 'echo "E2E tests disabled for this application"');
           }
-
-          this.scripts.set('regenerate', 'jhipster --with-entities --skip-install');
-
+        },
+        github() {
           const enableE2E = this.testFrameworks.includes('protractor');
           const githubConfigure = [
             `echo "::set-output name=e2e::${enableE2E}"`,
